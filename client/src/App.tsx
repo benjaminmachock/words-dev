25125f55728c9c44b1e3088cd16d922bc4fa4f49
Benjamin Machock <43613715+benjaminmachock@users.noreply.github.com>
2024-11-14 08:44:04 -0700

    test

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,10 @@
-import './App.css';
-import { Outlet } from 'react-router-dom';
+import "./App.css";
+import { Outlet } from "react-router-dom";
 
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
+//test
 
 function App() {
   return (
